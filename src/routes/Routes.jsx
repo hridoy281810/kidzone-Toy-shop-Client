a181a2bd30722d8cb93af5f48931c8be70274a65
hridoy281810 <hridoy01724281810@gmail.com>
2023-05-17 21:36:40 +0600

    environmet local file added

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+ {
+    path: '/',
+    element: <h1>akjsdfk</h1>
+ }
+
+
+])
+export default router;
