bfad92ae3403cf6a7c2d5a5e94ffe4d6a7368d7f
hridoy281810 <hridoy01724281810@gmail.com>
2023-05-19 20:05:54 +0600

    create a privet route

--- src/pages/ViewDetailsToys/ViewDetailsToys.jsx
+++ src/pages/ViewDetailsToys/ViewDetailsToys.jsx
@@ -3,36 +3,31 @@ import { Link, useLoaderData } from 'react-router-dom';
 import useTitle from '../../hooks/useTitle';
 
 const ViewDetailsToys = () => {
-  useTitle('KidZone | Details Toy') 
-   const toy = useLoaderData()
-   const {_id,category,pictureUrl,toyName, price, rating, seller,  availableQuantity,
+  useTitle('KidZone | Details Toy')
+  const toy = useLoaderData()
+  const {category, pictureUrl, toyName, price, rating, seller, availableQuantity,
     sellerEmail,
-    detailDescription} = toy;
-
-    return (
+    detailDescription } = toy;
+  return (
     <div className='container'>
-          
-          <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row gap-20">
-    <img src={pictureUrl} className="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 className="text-5xl font-bold">{toyName}</h1>
-      <p className="pt-4 text-xl "><strong>Category Name:</strong> {category}</p>
-      <p className="pt-4 text-xl "><strong>Seller Name:</strong> {seller}</p>
-      <p className="pt-4 text-xl "><strong>Seller Email:</strong> {sellerEmail}</p>
-      <p className="pt-4 text-xl "><strong>Price:</strong> ${price}</p>
-      <p className="pt-4 text-xl "><strong>Available Quantity:</strong> {availableQuantity} Pis</p>
-      <p className="pt-4 text-xl "><strong>Rating:</strong> {rating}</p>
-      <p className="pt-4 text-xl mb-4 w-[400px] "><strong>Description:</strong> {detailDescription} stars</p>
-
-      <Link to='/' className="btn btn-outline btn-secondary">Back To Home</Link>
-     
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row gap-20">
+          <img src={pictureUrl} className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="text-5xl font-bold">{toyName}</h1>
+            <p className="pt-4 text-xl "><strong>Category Name:</strong> {category}</p>
+            <p className="pt-4 text-xl "><strong>Seller Name:</strong> {seller}</p>
+            <p className="pt-4 text-xl "><strong>Seller Email:</strong> {sellerEmail}</p>
+            <p className="pt-4 text-xl "><strong>Price:</strong> ${price}</p>
+            <p className="pt-4 text-xl "><strong>Available Quantity:</strong> {availableQuantity} Pis</p>
+            <p className="pt-4 text-xl "><strong>Rating:</strong> {rating}</p>
+            <p className="pt-4 text-xl mb-4 w-[400px] "><strong>Description:</strong> {detailDescription} stars</p>
+            <Link to='/' className="btn btn-outline btn-secondary">Back To Home</Link>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    </div>
-       
-    );
+  );
 };
 
 export default ViewDetailsToys;
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import  { createContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
@@ -6,46 +6,46 @@ import app from '../firebase/firebase.config';
 export const AuthContext = createContext(null)
 const auth = getAuth(app)
 const googleProvider = new GoogleAuthProvider();
-const AuthProvider = ({children}) => {
-const [user, setUser] = useState(null)
-const [loading, setLoading] = useState(true)
- const createUser = (email, password) => {
-    setLoading(true)
- return createUserWithEmailAndPassword(auth, email, password)
- }
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
     const loginUser = (email, password) => {
-     setLoading(true)
- return signInWithEmailAndPassword(auth, email, password)
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
     }
     const googleLogin = () => {
- return signInWithPopup(auth, googleProvider);
+        return signInWithPopup(auth, googleProvider);
     }
-const logOut = () => {
-    setLoading(true)
- return signOut(auth);
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth);
     }
     useEffect(() => {
-const unsubscribe = onAuthStateChanged(auth, loggedUser => {
-setUser(loggedUser)
-    setLoading(false)
-     })
- return () => {
-     unsubscribe()
+        const unsubscribe = onAuthStateChanged(auth, loggedUser => {
+            setUser(loggedUser)
+            setLoading(false)
+        })
+        return () => {
+            unsubscribe()
         }
     }, [])
-const authInfo = {
-    user,
-    loading,
-    setLoading,
-    createUser,
-    loginUser,
-    googleLogin,
-    logOut
+    const authInfo = {
+        user,
+        loading,
+        setLoading,
+        createUser,
+        loginUser,
+        googleLogin,
+        logOut
     }
     return (
-          <AuthContext.Provider value={authInfo}>
-                 {children}
-          </AuthContext.Provider>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
 };
 export default AuthProvider;
